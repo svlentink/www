4941b652b8d71f76a8e20d6d6f295a791e30cf1a
svlentink <sv@lent.ink>
2019-10-21 14:57:08 +0000

    updated to prototype

--- cdn.lent.ink/js/03_hack/hack01_gmail.js
+++ cdn.lent.ink/js/03_hack/hack01_gmail.js
@@ -4,7 +4,7 @@
 
   var base2 = new SVL.math.Base(2)
 
-  var Gmail = function (username) {
+  var Gmail = function (username, case_insensitive) {
     if (typeof username !== 'string') throw new TypeError()
     log.enter('Gmail', arguments)
     // emails can contain +; e.g. userJohn+facebook@gmail.com can be handy to filter emails
@@ -18,10 +18,13 @@
     uname = uname.toLowerCase()
     log.info('Created new gmail object', {username: uname})
     this.getUsername = function () { return uname }
+    this.isCaseInsensitive = function () { return case_insensitive ? true : false }
   }
 
   Gmail.prototype.getLength = function () {
     log.enter('Gmail.getLength')
+    var uname = this.getUsername()
+    if (this.isCaseInsensitive()) return Math.pow(2, uname.length)
     // if the username has 4 chars, e.g. john
     // we can write all letters with upper or lower case
     // i.e. they are binary, which makes john possible to write in 2^4 ways.
@@ -30,7 +33,6 @@
     // and in the end, we have @gmail and @googlemail, which is 2^1
     // which brings us to:
     // 2^(amount of letters, no numbers) * 2^(username length, with numbers etc.)
-    var uname = this.getUsername()
     var letterCount = 0
     for (var i = 0; i < uname.length;i++) letterCount += uname[i].isLetter()
 
@@ -41,13 +43,13 @@
     log.enter('Gmail.getIndex', arguments)
     if (index >= this.getLength()) throw new Error()
     var arr = base2.intToArr(index)
-    var domain = arr.pop() ? '@googlemail.com' : '@gmail.com' // domains .de and .co redirect to gmail
+    var domain = arr.pop() ? '@googlemail.com' : '@gmail.com' // domains .de and .fr redirect to gmail
     var uname = this.getUsername().split('')
 
     while (uname.length) {
       var char = uname.pop()
       // only when char is a letter, pop the Array
-      if (char.isLetter() && arr.length && arr.pop()) char = char.toUpperCase()
+      if (!this.isCaseInsensitive() && char.isLetter() && arr.length && arr.pop()) char = char.toUpperCase()
       var dot = (arr.length && arr.pop()) ? '.' : ''
 
       domain = dot + char + domain
@@ -57,28 +59,27 @@
 
   /**
   * Loop over email address and perform a function
-  * @param {string} username
   * @param {function} func Function with one argument; the current email
   * @param {int} delay (optional) Timeout in milisec
   * @param {int} offset (optional) From which int to start
   * @param {int} amount (optional) default is all possibilities
   */
-  Gmail.foreach = function (username, func, delay, offset, amount) {
+  Gmail.prototype.forEach = function (func, delay, offset, amount) {
     log.enter('Gmail.foreach', {offset: offset, amount: amount})
     var dly = delay || 0
     var indx = offset || 0
     // check if amount is zero
-    if (arguments.length > 4 && !amount) return
+    if (arguments.length > 3 && !amount) return
 
-    var gmail = new Gmail(username)
-    if (indx >= gmail.getLength()) return
-    var mail = gmail.getIndex(indx)
+    if (indx >= this.getLength()) return
+    var mail = this.getIndex(indx)
 
     func(mail) // preform the function
-    glob.setTimeout(function () { // set delay for next interval
-      if (amount) Gmail.foreach(username, func, dly, indx + 1, amount - 1)
-      else Gmail.foreach(username, func, dly, indx + 1)
-    }, dly)
+    var obj = this
+    glob.setTimeout(function (obj) { // set delay for next interval
+      if (amount) this.forEach(func, dly, indx + 1, amount - 1)
+      else this.forEach(func, dly, indx + 1)
+    }.bind(this), dly)
   }
 
   Gmail.exampleCode = function (uname) {
@@ -128,7 +129,8 @@
     }
 
     var delay = 1000 * 60 * 20
-    Gmail.foreach(uname, funcWithCookie, delay)
+    var gmail = new Gmail(uname, true)
+    gmail.foreach(uname, funcWithCookie, delay)
   }
 
   SVL.set(['hack', 'Gmail'], Gmail)
