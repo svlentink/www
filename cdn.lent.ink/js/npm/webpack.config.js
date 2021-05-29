5e630dc6bf6908b8bfde0ebb13aa5aef11088c36
SanderLentink.nl <sander@lent.ink>
2021-05-29 12:09:47 +0200

    fallback false

--- cdn.lent.ink/js/npm/webpack.config.js
+++ cdn.lent.ink/js/npm/webpack.config.js
@@ -34,9 +34,23 @@ module.exports = [
   optimization: {
     minimize: true
   },
+  // https://stackoverflow.com/questions/64557638/how-to-polyfill-node-core-modules-in-webpack-5
+  resolve: {
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+    } 
+  },
   plugins: [
-		new NodePolyfillPlugin()
-	],
+    new NodePolyfillPlugin()
+  ],
 },
 {
   mode: "production",
